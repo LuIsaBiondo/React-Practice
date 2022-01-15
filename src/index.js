fb37733348647d342e3b8129c2b5ed8e23033080
Luciana <lucianabiondolillo@gmail.com>
2022-01-15 11:00:34 -0300

    Mi lista

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react'; // importamos react
 import ReactDOM from 'react-dom'; // nos permite renderizar en el DOM
-const datos = [nombre, apellido, email, telefono];
-const listItems = datos.map((dato) =>
-  <li>{dato}</li>
-);
-ReactDOM.render(
-    <ul>{listItems}</ul>,
-    document.getElementById('root')
-  );
+function App() {
+    const people = [
+      { name: 'chris' },
+      { name: 'nick' }
+    ];
+  
+    return (
+      <div>
+        {people.map(person => (
+          <p>{person.name}</p>
+        ))}
+      </div>
+    );
+  }
