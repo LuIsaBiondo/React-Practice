99f2f5df89f68bb116d0915ad2a9b3d43681de1e
Fernando A. Gonzalez <fagonzalez.nt@gmail.com>
2022-01-19 18:54:41 -0300

    DataLista.js formatted

--- src/DatosLista.js
+++ src/DatosLista.js
@@ -1,48 +1,47 @@
 
 const list = [
-    {
-      email: 'lucianabiondolillo@gmail.com',
-      firstname: 'Luciana',
-      lastname: 'Biondolillo',
-      telefono: 2616407710,
-    },
-    {
-      email: 'valecruz@gmail.com',
-      firstname: 'Valentina',
-      lastname: 'Cruz',
-      telefono: 2613316610,
-    },
-    {
-        email: 'wencepuelles@gmail.com',
-        firstname: 'Wenceslao',
-        lastname: 'Puelles',
-        telefono: 2613316611,
-      },
-      {
-        email: 'agustinlucero@gmail.com',
-        firstname: 'Agustin',
-        lastname: 'Lucero',
-        telefono: 2615239610,
-      },
-      {
-        email: 'agustinaboedo@gmail.com',
-        firstname: 'Agustina',
-        lastname: 'Boedo',
-        telefono: 2615239123,
-      },
-      {
-        email: 'tomaslucero@gmail.com',
-        firstname: 'Tomas',
-        lastname: 'Lucero',
-        telefono: 2615239690,
-      },
-      {
-        email: 'fagonzalez.nt@gmail.com',
-        firstname: 'Fer',
-        lastname: 'Gonzalez',
-        telefono: 33922052,
-      },
-  ];
+  {
+    email: 'lucianabiondolillo@gmail.com',
+    firstname: 'Luciana',
+    lastname: 'Biondolillo',
+    telefono: 2616407710,
+  },
+  {
+    email: 'valecruz@gmail.com',
+    firstname: 'Valentina',
+    lastname: 'Cruz',
+    telefono: 2613316610,
+  },
+  {
+    email: 'wencepuelles@gmail.com',
+    firstname: 'Wenceslao',
+    lastname: 'Puelles',
+    telefono: 2613316611,
+  },
+  {
+    email: 'agustinlucero@gmail.com',
+    firstname: 'Agustin',
+    lastname: 'Lucero',
+    telefono: 2615239610,
+  },
+  {
+    email: 'agustinaboedo@gmail.com',
+    firstname: 'Agustina',
+    lastname: 'Boedo',
+    telefono: 2615239123,
+  },
+  {
+    email: 'tomaslucero@gmail.com',
+    firstname: 'Tomas',
+    lastname: 'Lucero',
+    telefono: 2615239690,
+  },
+  {
+    email: 'fagonzalez.nt@gmail.com',
+    firstname: 'Fer',
+    lastname: 'Gonzalez',
+    telefono: 33922052,
+  },
+];
 
-
-  export default list;
+export default list;
