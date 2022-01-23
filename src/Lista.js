3cd9bfde98fa7ab3d3dc04540639cc71422ca475
Luciana <lucianabiondolillo@gmail.com>
2022-01-23 20:33:55 -0300

    Vinculando los datos a la tabla

--- src/Lista.js
+++ src/Lista.js
@@ -5,6 +5,7 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
