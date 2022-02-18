374efd3ec9c33e040c325b54d809b4b62ea310a0
Luciana <lucianabiondolillo@gmail.com>
2022-02-18 13:30:11 -0300

    Texfield validation error

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,17 +3,16 @@ import { Paper, TextField, Grid } from "@mui/material";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import Link from "@mui/material/Link";
-import Checkbox from '@mui/material/Checkbox';
-import Validacion from '../components/Validacion';
+import Checkbox from "@mui/material/Checkbox";
+import Box from "@mui/material/Box";
 
-
-const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 const Login = () => {
   const paperStyle = {
     padding: 20,
     height: "70vh",
-    width: 200,
+    width: 219,
     margin: "20px auto",
   };
   const btnStyle = { margin: "8px 0", padding: 10, height: "70vh" };
@@ -21,23 +20,30 @@ const Login = () => {
     <Paper elevation={10} style={paperStyle} align="center">
       <h2>Sign in</h2>
       <Grid align="center" />
-      <TextField
-        id="standard-basic"
-        label="Username"
-        placeholder="Enter username"
-        fullWidth
-        required
-        margin="20px"
-      />
-      <TextField
-        id="standard-basic"
-        label="Password"
-        placeholder="Enter password"
-        type="password"
-        fullWidth
-        required
-      />
-
+      <Box
+        component="form"
+        sx={{
+          "& .MuiTextField-root": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          id="standard-basic"
+          label="Username"
+          placeholder="Enter username"
+          fullWidth
+          required
+        />
+        <TextField
+          id="standard-basic"
+          label="Password"
+          placeholder="Enter password"
+          type="password"
+          fullWidth
+          required
+        />
+      </Box>
       <Stack spacing={2} direction="row">
         <Button variant="contained" color="secondary" fullWidth>
           Sing In
@@ -47,7 +53,8 @@ const Login = () => {
       <Link href="#">Sing Up</Link>
 
       <div>
-        <Checkbox {...label} defaultChecked />Remember Me
+        <Checkbox {...label} defaultChecked />
+        Remember Me
       </div>
     </Paper>
   );
